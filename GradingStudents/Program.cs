68aa4b1b28313ba64a300d08309465e02bb42cc3
Fidel Salazar <fidelsf91@gmail.com>
2020-11-02 15:01:59 -0600

    Added logic for rounding students' grades

--- GradingStudents/Program.cs
+++ GradingStudents/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace GradingStudents
 {
@@ -6,7 +8,39 @@ namespace GradingStudents
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = Result.gradingStudents(gradesCount, grades);
+
+            Console.WriteLine(String.Join("\n", result));
+        }
+
+        class Result
+        {
+            public static List<int> gradingStudents(int gradesCount, List<int> grades)
+            {
+                for(int i = 0; i < gradesCount; i++)
+                {
+                    if (grades[i] >= 38)
+                    {
+                        int dif = grades[i] % 5;
+                        if (dif >= 3)
+                        {
+                            grades[i] += (5 - dif);
+                        }
+                    }
+                }
+
+                return grades;
+            }
         }
     }
 }
